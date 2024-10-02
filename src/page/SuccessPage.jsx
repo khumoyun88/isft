04a3ad861874331a83113d54c93aca600f03ec7d
khumoyun davlatov <khumoyundavlatov@Mac-mini-khumoyun.local>
2024-10-02 21:48:10 +0500

    success page fixed

--- src/page/SuccessPage.jsx
+++ src/page/SuccessPage.jsx
@@ -18,12 +18,12 @@ const SuccessPage = () => {
           showContent ? "opacity-100 scale-100" : "opacity-0 scale-50"
         } transition-all duration-700 ease-out`}
       >
-        <img className="w-23 h-auto pb-40" src="/logo.svg" alt="logo" />
+        <img className="w-23 h-auto " src="/logo.svg" alt="logo" />
       </div>
       
       {/* Text */}
       <h1 className="text-2xl font-medium text-green-600 text-center mb-4">
-        Sizning ariznanngiz qabul qilindi. Tez orada siz bilan bog'lanamiz!
+        Sizning ariznangiz qabul qilindi. Tez orada siz bilan bog'lanamiz!
       </h1>
 
       {/* Animated Check Icon */}
